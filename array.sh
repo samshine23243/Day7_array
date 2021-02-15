706d743674c9692e6b32adce9bb64e7bf9a640a1
samshine <sameershinde23243@gmail.com>
2021-02-15 13:54:27 +0530

    example4 code added

--- array.sh
+++ array.sh
@@ -1,1 +1,10 @@
-echo "array problem"
+sum=0
+for ((i=1;i<=3;i++))
+do
+        randomNumber=$((1+$RANDOM%3))
+        arr[$i]=$randomNumber
+        sum=$(($sum+$randomNumber))
+done
+echo "numbers are : " ${arr[@]}
+echo "sum: " $sum
+
