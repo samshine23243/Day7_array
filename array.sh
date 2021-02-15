27cd24a0e3a1d4f5225dec28eb1024ab983e1ce7
samshine <sameershinde23243@gmail.com>
2021-02-15 13:51:22 +0530

    example3 code added

--- array.sh
+++ array.sh
@@ -1,1 +1,13 @@
-echo "array problem"
+read -p "Enter number : " num
+count=0
+for ((i=2;i<=$num;i++))
+do
+   while [ $(($num%$i)) -eq 0 ]
+   do
+                arr[$count]=$i
+      num=$(($num/$i))
+                count=$count+1
+   done
+done
+echo "Prime factors:" ${arr[@]}
+
